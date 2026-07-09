1d98b71fe1f58e61e2c2ae3d2349db9968d6dacc
jufinamore <jufinamore@gmail.com>
2026-07-09 12:54:53 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "multiplicador-videos-v12";
+var CACHE_NAME = "multiplicador-videos-v13";
 var SHELL_FILES = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (event) {
